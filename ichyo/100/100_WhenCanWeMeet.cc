2136e921253aef844a18c0a77c54d48ff152fa7e
TasukuTakahashi <tasukutakahashi@MacBook-Pro-for-TasukuTakahashi-2.local>
2014-07-18 15:45:00 +0900

    init

--- ichyo/100/100_WhenCanWeMeet.cc
+++ ichyo/100/100_WhenCanWeMeet.cc
@@ -15,9 +15,58 @@
 #define ALL(c) (c).begin(), (c).end()
 #define dump(a) cerr << #a << "=" << (a) << endl
 
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //t=min 
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //t=max
+
 using namespace std;
 
 int main() {
+	int N,Q;
+	int M,date;
+	bool dates[100][50]; //date N
+
+	while(cin >> N >> Q) {
+		if (N == 0 && Q == 0) break;
+		fill_n((bool *)dates,sizeof(dates)/sizeof(bool),false);
+		rep(n,N) {
+			cin >> M;
+			rep(m,M) { //Dates
+				cin >> date;
+				dates[date][n] = true;
+			}
+		}
+
+		/*
+		rep(n,N) {
+			rep(d,20) {
+				cout << dates[d][n];
+			}
+			cout << endl;
+		}
+		cout << endl; */
+
+		int cnt=0;
+		int max_cnt=0;
+		int max_d=0;
+		rep(d,100) {
+			cnt = 0;
+			rep(n,N) { //縦に見る
+				if(dates[d][n]) {
+					cnt++;
+				}
+			}
+			if (cnt > max_cnt) {
+				max_cnt = cnt;
+				max_d = d;
+			}
+		}
+		if (max_cnt >= Q) {
+			cout << max_d << endl;
+		}
+		else {
+			cout << 0 << endl;
+		}
+	}
 	
 
 	return 0;
--- ichyo/100/tmp.cc
+++ ichyo/100/tmp.cc
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<map>
 #include<vector>
+#include<list>
 #include<algorithm>
 #include<cmath>
 #include<iomanip>
@@ -8,18 +9,28 @@
 #include<climits>
 #include<ctime>
 #include<cstring>
-#include<sstream>
 #define REP(i,p,n) for(int i=p;i<(int)(n);i++)
 #define rep(i,n) REP(i,0,n)
 #define rep_split(tok,a_str,re) for(char *tok = strtok((char *)a_str.c_str(),re); tok != NULL; tok = strtok(NULL,re))
 #define ALL(c) (c).begin(), (c).end()
 #define dump(a) cerr << #a << "=" << (a) << endl
-#define DUMP(list) cout << "{ "; for(auto nth : list){ cout << nth << " "; } cout << "}" << endl;
-template<class T> void chmin(T &t, T f) { if (t > f) t = f; } //t=min
-template<class T> void chmax(T &t, T f) { if (t < f) t = f; } //t=max
+
 using namespace std;
 
 int main() {
+	int a[31];
+	int b[31][100];
+	dump(sizeof(a)/sizeof(int));
+	dump(sizeof(b)/sizeof(int));
+
+	fill_n((int *)b, sizeof(b)/sizeof(int), 7);
+	rep(j,20) {
+		rep(i,30) {
+			cout << b[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 
 	return 0;
 }
